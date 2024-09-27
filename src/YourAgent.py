1ac8b9cb7e4f4ec26d681863c7c2bf7d2455b237
Owen Wang <owenwang0099@gmail.com>
2024-09-27 16:24:25 -0400

    solution

--- src/YourAgent.py
+++ src/YourAgent.py
@@ -3,6 +3,7 @@ from src.DriveState import DriveState
 from src.Constants import DriveMove, SensorData
 from src.Utils import manhattan_dist_2D
 
+
 class YourAgent(DriveInterface):
 
     def __init__(self, game_id: int, is_advanced_mode: bool):
@@ -15,6 +16,7 @@ class YourAgent(DriveInterface):
         """
         self.game_id = game_id
         self.need_to_find_target_pod = is_advanced_mode
+        self.target_pod_acquired = False
 
     # This is the main function the simulator will call each turn
     def get_next_move(self, sensor_data: dict) -> DriveMove:
@@ -48,4 +50,126 @@ class YourAgent(DriveInterface):
             DriveMove.LIFT_POD – If a pod is in the same tile, pick it up. The pod will now move with the drive until it is dropped
             DriveMove.DROP_POD – If a pod is in the same tile, drop it. The pod will now stay in this position until it is picked up
         """
-        pass
+
+        x_pos, y_pos = sensor_data[SensorData.PLAYER_LOCATION]
+
+        goals = sensor_data[SensorData.GOAL_LOCATIONS]
+        drive_locs = sensor_data[SensorData.DRIVE_LOCATIONS]
+        pod_locs = sensor_data[SensorData.POD_LOCATIONS]
+
+        # Advanced Mode
+        if self.need_to_find_target_pod:
+            # Go to target pod
+            pod_x, pod_y = sensor_data[SensorData.TARGET_POD_LOCATION]
+            dist_x, dist_y = self.get_xy_distance(x_pos, y_pos, pod_x, pod_y)
+
+            # If arrived at target pod, pick up pod
+            if self.target_pod_acquired:
+                pass
+            if dist_x == 0 and dist_y == 0 and not self.target_pod_acquired:
+                self.target_pod_acquired = True
+                return DriveMove.LIFT_POD
+
+            # Send move instruction
+            if dist_x > 0 and not self.drive_at_loc(drive_locs, x_pos + 1, y_pos):
+                return DriveMove.RIGHT
+            elif dist_x < 0 and not self.drive_at_loc(drive_locs, x_pos - 1, y_pos):
+                return DriveMove.LEFT
+            if dist_y > 0 and not self.drive_at_loc(drive_locs, x_pos, y_pos + 1):
+                return DriveMove.UP
+            elif dist_y < 0 and not self.drive_at_loc(drive_locs, x_pos, y_pos - 1):
+                return DriveMove.DOWN
+
+        # Find nearest goal
+        shortest_dist = -1
+        goal_x = -1
+        goal_y = -1
+        for goal in goals:
+            dist_x, dist_y = self.get_xy_distance(x_pos, y_pos, goal[0], goal[1])
+            dist = self.get_raw_distance(dist_x, dist_y)
+
+            if shortest_dist == -1:
+                shortest_dist = dist
+                goal_x = dist_x
+                goal_y = dist_y
+            elif shortest_dist > dist:
+                shortest_dist = dist
+                goal_x = dist_x
+                goal_y = dist_y
+
+        # Send move instruction
+        if abs(goal_x) > abs(goal_y):
+            cmd = self.move_to_x_target(x_pos, y_pos, goal_x, drive_locs, pod_locs)
+            if cmd:
+                return cmd
+            cmd = self.move_to_y_target(x_pos, y_pos, goal_y, drive_locs, pod_locs)
+            if cmd:
+                return cmd
+        cmd = self.move_to_y_target(x_pos, y_pos, goal_y, drive_locs, pod_locs)
+        if cmd:
+            return cmd
+        cmd = self.move_to_x_target(x_pos, y_pos, goal_x, drive_locs, pod_locs)
+        if cmd:
+            return cmd
+
+        # Move out of way of obstacle
+        if goal_x != 0:
+            # Move up/down
+            if not self.obstacle_at_loc(
+                x_pos, y_pos + 1, drive_locs, pod_locs, self.need_to_find_target_pod
+            ):
+                return DriveMove.UP
+            if not self.obstacle_at_loc(
+                x_pos, y_pos - 1, drive_locs, pod_locs, self.need_to_find_target_pod
+            ):
+                return DriveMove.DOWN
+        if goal_y != 0:
+            # Move right/left
+            if not self.obstacle_at_loc(
+                x_pos + 1, y_pos, drive_locs, pod_locs, self.need_to_find_target_pod
+            ):
+                return DriveMove.RIGHT
+            if not self.obstacle_at_loc(
+                x_pos - 1, y_pos, drive_locs, pod_locs, self.need_to_find_target_pod
+            ):
+                return DriveMove.LEFT
+
+        return DriveMove.NONE
+
+    def get_xy_distance(self, x1, y1, x2, y2):
+        return x2 - x1, y2 - y1
+
+    def get_raw_distance(self, x, y):
+        return (x**2 + y**2) ** 0.5
+
+    def drive_at_loc(self, drive_locs, x, y):
+        return [x, y] in drive_locs
+
+    def pod_at_loc(self, pod_locs, x, y):
+        return [x, y] in pod_locs
+
+    def obstacle_at_loc(self, x, y, drive_locs, pod_locs, is_advanced):
+        if self.drive_at_loc(drive_locs, x, y):
+            return True
+        if is_advanced and self.pod_at_loc(pod_locs, x, y):
+            return True
+
+    def move_to_x_target(self, x_pos, y_pos, target_x, drive_locs, pod_locs):
+        if target_x > 0 and not self.obstacle_at_loc(
+            x_pos + 1, y_pos, drive_locs, pod_locs, self.need_to_find_target_pod
+        ):
+            return DriveMove.RIGHT
+        elif target_x < 0 and not self.obstacle_at_loc(
+            x_pos - 1, y_pos, drive_locs, pod_locs, self.need_to_find_target_pod
+        ):
+            return DriveMove.LEFT
+
+    def move_to_y_target(self, x_pos, y_pos, target_y, drive_locs, pod_locs):
+        if target_y > 0 and not self.obstacle_at_loc(
+            x_pos, y_pos + 1, drive_locs, pod_locs, self.need_to_find_target_pod
+        ):
+            return DriveMove.UP
+        elif target_y < 0 and not self.obstacle_at_loc(
+            x_pos, y_pos - 1, drive_locs, pod_locs, self.need_to_find_target_pod
+        ):
+            return DriveMove.DOWN
